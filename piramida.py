62ecd98ae8386b3c46c976483e93fc00769ecaa4
basic <basic@rseu.ru>
2020-09-30 10:27:09 +0300

    it's only test

---
+++ piramida.py
@@ -0,0 +1,9 @@
+height = int(input("Введите рост пирамиды"))
+for i in range(1, height+1):
+	for j in range(height - i):
+		print(" ", end="")      
+	for j in range(i, 0, -1):
+		print(j, end="")
+	for j in range(2, i+1):
+		print(j, end="")
+	print()
